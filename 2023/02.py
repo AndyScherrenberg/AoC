9f874e841a5b0c6852d1b2aec11a77f544940026
Andy Scherrenberg <AndyScherrenberg@users.noreply.github.com>
2023-12-02 12:35:21 +0100

    AoC 2023 D2

--- 2023/02.py
+++ 2023/02.py
@@ -3,15 +3,11 @@ import re
 with open('02.txt', 'r') as file:
     file = file.read()
     
-    
 instr = file.split("\n")
 
 p1,p2 = 0,0
 
-blue = 14
-red = 12
-green = 13
-
+red,green,blue = 12,13,14
 
 def get_cube_amount(cube):
     find = re.findall("\d* "+cube, g)
@@ -22,20 +18,11 @@ def get_cube_amount(cube):
     
 for line in instr:
     possible = True
-    #print(line)
     collection = line.split(":")
     gameid = int(collection[0].replace("Game ", "").replace(":",""))
-    #print(gameid)
     game = collection[1].split(";")
-    
-    highest_blue = 0
-    highest_red = 0
-    highest_green = 0
-    
+    highest_blue,highest_red,highest_green = 0,0,0
     for g in game:
-        #current_set = g.split(",")
-        #sets.append(g.split(","))
-        #for cs in current_set:
         red_amount = get_cube_amount("red")
         blue_amount = get_cube_amount("blue")
         green_amount =get_cube_amount("green")
@@ -47,10 +34,7 @@ for line in instr:
             highest_blue = blue_amount
         if(green_amount > green or red_amount >red or blue_amount > blue):
             possible = False
-        
     if (possible):
         p1 += gameid
     p2 += highest_blue * highest_red * highest_green
-    
-    #print(sets)
 print(p1,p2)
